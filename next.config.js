29b97705fc743ffd769fd319ed7f53f8bd8a5994
Ogundele Stephen Babatunde <ogundele370@gmail.com>
2023-07-23 15:20:04 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,46 @@
-module.exports = {
-  env: {
-    BASE_URL: "https://kreatesell.io/api/",
-    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6Le7-jQcAAAAAOHkoLvhdgAjcmfi2gcHjLKkCzYB",
-  },
+// Import bundle analyzer to enable compression
+const withBundleAnalyzer = require('@next/bundle-analyzer')
+
+module.exports = withBundleAnalyzer({
+  compress: true,  
+
+  // Base path for production
+  basePath: '/production-path',
+
+  // Asset prefix for CDN
+  assetPrefix: 'https://mycdn.com',
+
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ['res.cloudinary.com'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], 
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    path: '/_next/image',
+    loader: 'default'
   },
-};
+
+  // Custom headers  
+  headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains', 
+          }
+        ]
+      }
+    ]
+  },
+
+  // Redirects
+  async redirects() {
+    return [
+      {
+        source: '/old-path',
+        destination: '/new-path',
+        permanent: false
+      }
+    ]
+  } 
+})
